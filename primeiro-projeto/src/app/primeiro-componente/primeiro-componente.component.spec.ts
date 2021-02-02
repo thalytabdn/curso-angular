fe9cff5b4adb936453f5e2393a54a5197ee84cf1
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2021-02-02 17:55:02 -0300

    add primeiro componente

---
+++ primeiro-projeto/src/app/primeiro-componente/primeiro-componente.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrimeiroComponenteComponent } from './primeiro-componente.component';
+
+describe('PrimeiroComponenteComponent', () => {
+  let component: PrimeiroComponenteComponent;
+  let fixture: ComponentFixture<PrimeiroComponenteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrimeiroComponenteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrimeiroComponenteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ primeiro-projeto/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'primeiro-componente',
+  templateUrl: './primeiro-componente.component.html',
+  styleUrls: ['./primeiro-componente.component.css']
+})
+export class PrimeiroComponenteComponent {
+
+
+}
