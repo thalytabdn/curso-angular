d5cb01f589b8c09b93082b0d1a2ec3420a1a65dc
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2021-02-02 17:07:12 -0300

    -

---
+++ hello-typescript/main.ts
@@ -0,0 +1,29 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x,y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+
+var numeros = [1,2,3];
+numeros.map(function (valor) {
+    return valor * 2
+}); 
+
+numeros.map(valor => {
+    return valor * 2
+});
+
+class Matematica {
+    soma(x,y){
+        return x + y;
+    }
+}
+
+var n1: any = 'sads';
+n1 = 4;
+
+//tsc <nome-arquivo>.tsc
