d37a66abd75f26996737ed9c95c2f7243e2cade5
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2021-02-02 22:23:21 -0300

    creating modules

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 
 @NgModule({
+  // listar todos os componentes, diretivas e pipes a serem utilizados no module
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent
   ],
+  // outros modules que serão utilzados, ou modules que os componentes necessitam
   imports: [
     BrowserModule,
+    CursosModule,
     AppRoutingModule
   ],
+  // serviços que ficaram disponíveis para todos os componentes declarados neste module
   providers: [],
-  bootstrap: [AppComponent]
+  // componente que deve ser instanciado quando o app é iniciado
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
